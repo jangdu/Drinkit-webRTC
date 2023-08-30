c75e7316193358aa5c5c4efb22df2688fbfded75
yhjs1211 <yhjs1211@gmail.com>
2023-08-30 22:29:58 +0900

    feat: redis_init() update
    > 기초 데이터 생성 로직 추가

--- src/redis.ts
+++ src/redis.ts
@@ -7,6 +7,24 @@ export class RedisClient {
 
   async init() {
     await this.client.connect();
+    const roomCnt = await this.client.get('roomCnt');
+    const chatRooms = await this.client.json.get('chatRooms');
+
+    const chatRoomsObj = {
+      '2': [],
+      '3': [],
+      '4': [],
+    };
+
+    if (!roomCnt && !chatRooms) {
+      await this.client.set('roomCnt', '1');
+      await this.client.json.set('chatRooms', '$', chatRoomsObj);
+    } else if (!roomCnt) {
+      await this.client.set('roomCnt', '1');
+    } else if (!chatRooms) {
+      await this.client.json.set('chatRooms', '$', chatRoomsObj);
+    }
+
     return;
   }
 }
